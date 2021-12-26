24b7088698c3be43b4be1cf3ba3dd3f5a64e1cc9
Agustin Lautaro Escobar <agustinescobar@hotmail.com.ar>
2021-12-26 18:38:13 -0300

    Instaladas dependencias e iniciado composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'df843afedec704cf16249822a617edf0927c56ec',
+        'name' => 'juandelatorre/up-task_mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'df843afedec704cf16249822a617edf0927c56ec',
+            'dev_requirement' => false,
+        ),
+    ),
+);
