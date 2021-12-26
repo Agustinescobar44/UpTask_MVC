2c4e09d4ce5d650f8c21265119f438672c9f38d0
Agustin Lautaro Escobar <agustinescobar@hotmail.com.ar>
2021-12-26 18:54:06 -0300

    Agreados metodos y vistas para el login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,8 +2,40 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController{
-    public static function login(){
-        echo "Desde Login";
+    public static function login(Router $router){
+        if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+        }
+        echo "Desde LOGIN";
+
+        $router->render('login/crear',[
+            
+        ]);
+    }
+    public static function logout(){
+        echo "Desde logout";
+    }
+    public static function crear(Router $router){
+        if($_SERVER['REQUEST_METHOD'] === "POST"){
+            
+        }
+        echo "Desde Crear";
+
+        $router->render('login/crear',[
+
+        ]);
+    }
+    public static function olvide(Router $router){
+        if($_SERVER['REQUEST_METHOD'] === "POST"){
+            
+        }
+        echo "Desde olvide";
+
+        $router->render('login/olvide',[
+            
+        ]);
     }
 }
