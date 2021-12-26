c10ca32a6232f081681acc039a5946bd0fb9ce68
Agustin Lautaro Escobar <agustinescobar@hotmail.com.ar>
2021-12-26 18:47:12 -0300

    Creado routeo para el login

--- public/index.php
+++ public/index.php
@@ -2,9 +2,21 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+// login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+// logout
+$router->get('/logout', [LoginController::class, 'logout']);
+//olvido password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+//crear cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
 
 
 
